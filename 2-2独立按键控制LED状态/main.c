04d856a214d3cb5bbb986ee79fb04f7f7c9c2827
Graypigeonhgh <111276643+Graypigeonhgh@users.noreply.github.com>
2022-10-24 12:11:10 +0800

    Add files via upload

---
+++ 2-2独立按键控制LED状态/main.c
@@ -0,0 +1,31 @@
+#include <REGX52.H>
+
+void Delay1ms(unsigned int xms)		//@12.000MHz
+{
+	unsigned char i, j;
+	while(xms)
+	{
+	i = 2;
+	j = 239;
+	do
+	{
+		while (--j);
+	} while (--i);
+	xms--;
+
+	}
+}
+
+void main()
+{
+	while(1)
+	{
+		if(P3_1==0)
+		{
+			Delay1ms(20);
+			while(P3_1==0);
+			Delay1ms(20);
+			P2_0=~P2_0;
+		}
+	}
+}
